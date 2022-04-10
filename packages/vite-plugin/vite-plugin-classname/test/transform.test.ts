2560f8f7239a7c4db9b50ad06c7ed6f8724e2af0
Spencer17x <1253478653@qq.com>
2022-04-10 17:18:28 +0800

    feat(vite-plugin-classname): add transform unit test

--- packages/vite-plugin/vite-plugin-classname/test/transform.test.ts
+++ packages/vite-plugin/vite-plugin-classname/test/transform.test.ts
@@ -1,5 +1,57 @@
-describe('add test', function () {
-  it('add function test', function () {
-    expect(1 + 2).toEqual(3)
+import { transformCSS, transformTSX } from '../src/transform';
+
+describe('transform', function () {
+  it('transform css', function () {
+    const code = `.app .h1 {
+      color: red;
+    }
+    .app .h2 {
+      color: blue;
+    }
+    .app .h3 {
+      color: green;
+    }`;
+    expect(transformCSS('dd', code)).toMatchInlineSnapshot(`".dd-app .dd-h1{color:red}.dd-app .dd-h2{color:blue}.dd-app .dd-h3{color:green}"`);
+  });
+
+  it('transformTSX', function () {
+    const oneClassNameCode = `{
+      className: "app",
+      children: []
+    }`;
+    const multipleClassNameCode = `{
+      className: "app",
+      children: [
+        {
+          className: "h1",
+          children: []
+        },
+        {
+          className: "h2",
+          children: []
+        }
+      ]
+    }`;
+    expect(transformTSX('dd', oneClassNameCode)).toMatchInlineSnapshot(`
+"{
+      className: \\"dd-app\\",
+      children: []
+    }"
+`);
+    expect(transformTSX('dd', multipleClassNameCode)).toMatchInlineSnapshot(`
+"{
+      className: \\"dd-app\\",
+      children: [
+        {
+          className: \\"dd-h1\\",
+          children: []
+        },
+        {
+          className: \\"dd-h2\\",
+          children: []
+        }
+      ]
+    }"
+`);
   });
 });
